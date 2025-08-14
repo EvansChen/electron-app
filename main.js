3710af3a653038bcf9f4363983daeffde5be61b7
我自己 <czf-0754@126.com>
2025-08-14 11:01:53 +0800

    Initial commit: Basic Electron app setup

---
+++ main.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// 保持对窗口对象的全局引用，如果不这样做，当 JavaScript 对象被垃圾回收时，窗口会自动关闭。
+let mainWindow;
+
+function createWindow() {
+  // 创建浏览器窗口
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // 加载应用的 index.html
+  mainWindow.loadFile('index.html');
+
+  // 打开开发者工具（可选）
+  // mainWindow.webContents.openDevTools();
+
+  // 当窗口被关闭时，这个事件会被触发
+  mainWindow.on('closed', () => {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    mainWindow = null;
+  });
+}
+
+// Electron 会在初始化后并准备创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow);
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // 在 macOS 上，当单击 dock 图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// 在这个文件中，你可以包含应用程序剩余的所有部分的代码，
+// 也可以把它们放在分离的文件中，然后用 require 导入。
